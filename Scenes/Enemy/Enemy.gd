66b07f88e5aec5acf8e9bab627464fc3393c492e
pyxn <51064667+pyxn@users.noreply.github.com>
2024-08-05 16:09:26 -0400

    38/Making a base

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -2,6 +2,10 @@ extends PathFollow3D
 
 @export var speed: float = 2.414
 
+@onready var base = get_tree().get_first_node_in_group("Base")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * speed
+	if progress_ratio == 1.0:
+		base.take_damage()
